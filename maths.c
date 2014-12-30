64e013df9fc6ab03f50497d9af84cbb84b2656b6
andrew <andrew@localhost>
2014-12-30 16:39:17 +1300

    updated to latest math.? from ATIRE

--- maths.c
+++ maths.c
@@ -1,5 +1,11 @@
 #include "maths.h"
 
+
+unsigned long ANT_ffs_nonzero_table32[] =
+{
+0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+};
+
 unsigned long ANT_floor_log2_byte[] =
 {
 0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
